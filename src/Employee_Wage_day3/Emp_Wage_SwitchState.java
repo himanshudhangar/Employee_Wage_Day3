7831bce8ff16c71cf6d1bf38955b60e79a7c913a
Git with himanshu <dhangarhimanshu450@gmail.com>
2022-03-24 23:12:55 +0530

    added switch case

---
+++ src/Employee_Wage_day3/Emp_Wage_SwitchState.java
@@ -0,0 +1,30 @@
+package Employee_Wage_day3;
+
+public class Emp_Wage_SwitchState {
+
+	
+		public static final int PT=1;
+		public static final int FT=2;
+		public static final int ERPH=20;
+		
+			public static void main(String[] args) {
+				int EH=0;
+				int EW=0;
+				
+				int empcheck=(int) Math.floor(Math.random()*10)%3;
+				
+			switch(empcheck) {
+			case PT:
+				EH=4;
+				break;
+			case FT:
+				EH=8;
+				break;
+			default:
+				EH=0;
+			}
+			EW=EH*ERPH;
+			System.out.println("EMP Wage "+ EW);
+	}
+
+}
