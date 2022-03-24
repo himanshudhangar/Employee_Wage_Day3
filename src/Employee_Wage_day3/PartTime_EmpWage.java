4a63b130a2074356f885f92738166df3232873b5
Git with himanshu <dhangarhimanshu450@gmail.com>
2022-03-24 23:05:19 +0530

    added part time wage

---
+++ src/Employee_Wage_day3/PartTime_EmpWage.java
@@ -0,0 +1,30 @@
+package Employee_Wage_day3;
+
+public class PartTime_EmpWage {
+
+	public static void main(String[] args) {
+		int pt=1;
+		int ft=2;
+		int erph=20;
+		
+		int eh=0;
+		int ew=0;
+		
+		double empcheck=Math.floor(Math.random()*10)%2;
+		
+		if(empcheck==pt)
+		{
+			eh=4;
+			
+		}else if(empcheck==ft) {
+			eh=8;
+			
+			}else {
+				eh=0;
+				ew=eh*erph;
+				System.out.println("EmpWage: "+ew);
+				
+			}
+	}
+
+}
