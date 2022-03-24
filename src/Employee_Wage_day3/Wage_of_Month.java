6d67c756f588f6f9c4c12edca704fac4ded5e90e
Git with himanshu <dhangarhimanshu450@gmail.com>
2022-03-24 23:18:16 +0530

    added wage of month

---
+++ src/Employee_Wage_day3/Wage_of_Month.java
@@ -0,0 +1,36 @@
+package Employee_Wage_day3;
+
+public class Wage_of_Month {
+	public static final int PT=1;
+	public static final int FT=2;
+	public static final int ERPH=20;
+	public static final int WorkingDays=20;
+	
+		public static void main(String[] args) {
+			int EH=0;
+			int EW=0;
+			int totalEW=0;
+			
+			for(int day=0;day<WorkingDays;day++)
+			{
+				int empcheck=(int) Math.floor(Math.random()*10)%3;
+			
+			
+		switch(empcheck) {
+		case PT:
+			EH=4;
+			break;
+		case FT:
+			EH=8;
+			break;
+		default:
+			EH=0;
+		}
+		EW=EH*ERPH;
+		System.out.println("EMP Wage "+ EW);
+						
+	}
+			System.out.println("Total Emp Wage "+ totalEW);
+
+}
+}
